68ae2ebdd1a61cfb61deef9e39a478daf66d61cc
Socvest <82335390+Socvest@users.noreply.github.com>
2023-10-02 15:43:21 +0100

    Update screen_stats.py

--- screen_stats.py
+++ screen_stats.py
@@ -2,5 +2,20 @@ import streamlit as st
 from st_screen_stats import st_screen_data
 st.set_page_config(layout="wide")
 
+js = f"""
+            <script>
+                stDecoration = parent.document.querySelectorAll('[data-testid="stDecoration"]')
+                stDecoration[0].remove();
+                stFooter = parent.document.querySelectorAll('footer');
+                stFooter[0].remove();
+
+                removeElstDecoration = parent.document.querySelectorAll('iframe[srcdoc*="stDecoration[0].remove()"]')[0].parentNode;
+                removeElstDecoration.style = 'display:none;'
+                
+            </script>
+
+            """
+st.components.v1.html(js, height=0, width=0)
+
 
 st.write(st_screen_data())
